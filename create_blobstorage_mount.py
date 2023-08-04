298f8104a20bede91901050c7cd418bee9bf81e5
FabricioMoraes-Dev <FabricioMoraes-Dev>
2023-08-04 13:31:37 +0000

    Initial commit FROM Azure Databricks

---
+++ create_blobstorage_mount.py
@@ -0,0 +1,28 @@
+# Databricks notebook source
+dbutils.help()
+
+# COMMAND ----------
+
+dbutils.secrets.help()
+
+# COMMAND ----------
+
+dbutils.secrets.listScopes()
+
+# COMMAND ----------
+
+dbutils.secrets.list('SCOPE_NAME')
+
+# COMMAND ----------
+
+dbutils.fs.mount(source = "wasbs://CONTAINER_NAME@STORAGE_ACCOUNT_NAME.blob.core.windows.net",
+                 mount_point = "/mnt/MOUNT_NAME",
+                 extra_configs = {'fs.azure.account.key.STORAGE_ACCOUNT_NAME.blob.core.windows.net':dbutils.secrets.get('SCOPE_NAME','SECRET_NAME')})
+
+# COMMAND ----------
+
+dbutils.fs.ls("/mnt")
+
+# COMMAND ----------
+
+dbutils.fs.unmount("/mnt/MOUNT_NAME")
